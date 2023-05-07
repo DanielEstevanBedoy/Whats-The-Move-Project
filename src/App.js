fa19e64bfaa21e33c1da1b743a69653dde702c07
Estevan Bedoy <destevanbedoy@gmail.com>
2023-05-07 06:37:40 -0700

    Added HomePage and Sidebar Component files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import HomePage from './pages/HomePage/HomePage.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is the best app</h1>
+      <HomePage/>
     </div>
   );
 }
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.css'
+
+function HomePage(){
+    return(
+        
+
+
+        <h1 className='title'>HomePage</h1> 
+    )
+}
+
+export default HomePage;
