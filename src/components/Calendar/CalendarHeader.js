5e876c52348e336d3129b88ee5deefa7f363dce9
Alex Choi <achoi2003@gmail.com>
2023-05-28 14:52:36 -0700

    Name change

--- src/components/Calendar/CalendarHeader.js
+++ src/components/Calendar/CalendarHeader.js
@@ -6,7 +6,7 @@ export default function CalendarHeader() {
         <header className="px-4 py-2 flex items-center">
             <img src={logo} alt="logo" className="mr-2 w-12 h-12"/>
         <h1 className="mr-10 text-xl text-gray-500">
-            Calendar
+            what's the move?
         </h1>
         <button className="border rounded py-2 px-4 mr-5">
             Today
