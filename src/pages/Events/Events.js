6f44cfdc17a4c850caaa9e6300fde0a003b1a1bc
AntG2 <anth0n9g@gmail.com>
2023-05-31 15:08:46 -0700

    5/31: Unstyled: List out all events before today's date
    In Events.js, add similar logic to Day.js to filter through saved events
    Obtain all events strictly before today's date, and list them out in the Events page

--- src/pages/Events/Events.js
+++ src/pages/Events/Events.js
@@ -1,13 +1,36 @@
+import React, { useContext, useEffect,  useState } from "react";
+import GlobalContext from "../../Context/GlobalContext";
+import dayjs from "dayjs";
 
 
+export default function Test() {
+    const [pastEvents, setPastEvents] = useState([]);
+    const {savedEvents} = useContext(GlobalContext);
+    const day = dayjs();
+    useEffect(() => {
+	const events = savedEvents.filter(
+	    (event) => dayjs(event.day).format("YYYYMMDD") < day.format("YYYYMMDD")
+	);
+//	events.sort();
+	setPastEvents(events);
+    }, [day]);
+    
 
-export default function Events(){
-    return(
-        <div>
-            <h2>Events page</h2>
-
-        </div>
-
-    );
-
+    
+    return (
+	<>
+	    <h1 className="flex flex-col items-center"> Events </h1>
+	    {pastEvents.map((event, index) => (
+	    <div key={index}>
+		{event.title}
+	    </div>
+	    ))}
+	</>
+	
+	);
 }
+
+//function compare( a, b )
+//{
+    
+//}
