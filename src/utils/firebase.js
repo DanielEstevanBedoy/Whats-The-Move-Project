9679061966be46547a94aae247b7f4f935128322
AntG2 <anth0n9g@gmail.com>
2023-06-01 09:36:23 -0700

    6/1: Add image uploading to each event in Events tab
    Currently buggy: firebase storage cannot be initialized
    error message:
    0--firebasestorage.googleapis.com: Failed to load resource: preflight response is not successful. Status code: 404
    console error message:
    [Error] Preflight response is not successful. Status code: 404
    [Error] XMLHttpRequest cannot load https://firebasestorage.googleapis.com/v0/b/fir-auth-databa.appspot.com/o?name=files%2F%24%7Bfile.name%7D due to access control checks.

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 import { getDatabase } from "firebase/database";
 import {GoogleAuthProvider,signInWithPopup} from 'firebase/auth'
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,3 +22,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const db = getDatabase(app); // initialize and export the database
+export const storage = getStorage(app);
