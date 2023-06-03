9baa2caefbeaf7dc276f6f589f5c8bb124ebcd02
AntG2 <anth0n9g@gmail.com>
2023-06-03 15:19:21 -0700

    6/3: Events tab modified to hold past and upcoming events
    Original split Past and Upcoming events tabs combined back into the Events tab
    The new Events function (manual addition) removed, adding events function is reserved solely for the Calendar tab
    Events tab now include 2 buttons to move to the Past Events and Upcoming Events page. The two pages can move between each other.

--- src/pages/Events/UpcomingEvents.js
+++ src/pages/Events/UpcomingEvents.js
@@ -1,9 +1,21 @@
 import React from "react";
 import Events from "./Planning.js";
+import PastEvents from "./PastEvents.js"
+import { Route, Routes, Link } from 'react-router-dom';
 
 export default function FutureEvents()
 {
     return (
-	<Events lookForward={true} />
+	<>
+	    <div>
+		<nav className="navbar">
+	            <Link to="/PastEvents" className="nav-link"><button className="nav-button">Past Events</button></Link>
+		</nav>
+		<Routes>
+	            <Route path="/PastEvents" element={<PastEvents/>} />
+		</Routes>  
+            </div>
+	    <Events lookForward={true} />
+	</>
     );
 }
