77b73f6eb2b22f0af1a4482e2982f0cf04c5cbce
Jason Wan <njasow@g.ucla.edu>
2023-06-03 20:40:51 -0700

    Fixed reload bug. Reloading page no longer gets rid of events on calendar

--- src/Context/ContextWrapper.js
+++ src/Context/ContextWrapper.js
@@ -1,29 +1,48 @@
 import React, { useReducer, useState, useEffect } from "react";
-import { ref, set, update, get, push } from "firebase/database";
+import { ref, set, update, get, push, remove, child } from "firebase/database";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth, db } from "../utils/firebase";
 import GlobalContext from "./GlobalContext";
 import dayjs from "dayjs";
 
 /* Reducer function takes current state and an action, and returns the new state */
+/* Reducer function takes current state and an action, and returns the new state */
 function savedEventsReducer(state, { type, payload }) {
   switch (type) {
     case "ADD_EVENT":
+      if (auth.currentUser) {
+        const userEventsRef = ref(db, `Users/${auth.currentUser.uid}/Events`);
+        set(userEventsRef, payload);  // Directly save the new event in Firebase
+      }
       return [...state, payload];
+
     case "REMOVE_EVENT":
-      // remove an event from the state where the event id matches the payload id
+      if (auth.currentUser) {
+        const userEventsRef = ref(db, `Users/${auth.currentUser.uid}/Events`);
+        const eventToRemoveRef = child(userEventsRef, payload);
+        remove(eventToRemoveRef);
+      }
       return state.filter((event) => event.id !== payload.id);
+      
     case "UPDATE_EVENT":
-      // update an event in the state where the event id matches the payload id
+      if (auth.currentUser) {
+        const userEventsRef = ref(db, `Users/${auth.currentUser.uid}/Events`);
+        const eventToUpdateRef = child(userEventsRef, payload.id);
+        update(eventToUpdateRef, payload);
+      }
       return state.map((event) => (event.id === payload.id ? payload : event));
+
     case "INIT_EVENTS":
       return payload;
+
     case "CLEAR_EVENTS":
       return [];
+
     default:
       throw new Error();
   }
 }
+
 
 /* Initializes the state for useReducer. It retrieves events saved in the realtime database, parses them, 
 and returns them. If there are no events in localStorage, it returns an empty array. */
@@ -31,9 +50,17 @@ async function initEvents() {
   if (auth.currentUser) {
     const userEventsRef = ref(db, `Users/${auth.currentUser.uid}/Events`);
     let parsedEvents = [];
-    await userEventsRef.once('value', (snapshot) => {
+    await get(userEventsRef).then((snapshot) => {
         const data = snapshot.val();
-        parsedEvents = data ? Object.values(data) : [];
+        if (data) {
+          // Convert the object into an array of events
+          parsedEvents = Object.keys(data).map(key => {
+            return {
+              ...data[key],
+              id: key // Ensure the id is included in each event
+            }
+          });
+        }
     });
     return parsedEvents;
   } else {
@@ -51,41 +78,12 @@ export default function ContextWrapper(props) {
    */
 
   const [initialized, setInitialized] = useState(true);
-
-  // useEffect(() => {
-  //   const unsubscribe = onAuthStateChanged(auth, (user) => {
-  //     if (user) {
-  //       // User is signed in, fetch the events
-  //       initEvents().then(events => {
-  //         dispatchEvent({ type: "INIT_EVENTS", payload: events });
-  //         setInitialized(false);
-  //     });
-  //     } else {
-  //       // No user is signed in, clear the events
-  //       dispatchEvent({ type: "CLEAR_EVENTS" });
-  //     }
-  //   });
-
-  //   return () => unsubscribe();
-  // }, []);
-
   const [monthIndex, setMonthIndex] = useState(dayjs().month());
   const [daySelected, setDaySelected] = useState(dayjs());
   const [showEventForm, setShowEventForm] = useState(false);
   const [selectedEvent, setSelectedEvent] = useState(null);
 
   const [savedEvents, dispatchEvent] = useReducer(savedEventsReducer, []);
-  // useEffect(() => {
-  //   initEvents().then(events => {
-  //     dispatchEvent({ type: "INIT_EVENTS", payload: events });
-  //     setInitialized(true);
-  //   });
-  // }, []);
-
-  /* The function passed to useEffect will run everytime savedEvents changes */
-  // useEffect(() => {
-  //   localStorage.setItem("savedEvents", JSON.stringify(savedEvents));
-  // }, [savedEvents]);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -108,13 +106,13 @@ export default function ContextWrapper(props) {
       get(userEventsRef).then((snapshot) => {
         const data = snapshot.val();
         // Check if the user has any events in the database
-        if (data) {
-          // If they do, update the events with the current savedEvents
-          update(userEventsRef, savedEvents);
-        } else {
-          // If they don't, set the events to the current savedEvents
-          set(userEventsRef, savedEvents);
-        }
+        set(userEventsRef, savedEvents);
+        // if (data) {
+        //   // If they do, update the events with the current savedEvents
+        //   update(userEventsRef, savedEvents);
+        // } else {
+        //   // If they don't, set the events to the current savedEvents
+        // }
       });
     }
   }, [savedEvents, initialized]);
@@ -125,20 +123,6 @@ export default function ContextWrapper(props) {
         setSelectedEvent(null);
     }
   }, [showEventForm]);
-
-  /*
-  useEffect(() => {
-    if(selectedEvent) {
-        setTitle(selectedEvent.title);
-        setDescription(selectedEvent.description);
-        setSelectedLabel(labels.find((lbl) => lbl === selectedEvent.label));
-    } else {
-        setTitle("");
-        setDescription("");
-        setSelectedLabel(labels[0]);
-    }
-}, [selectedEvent]);
-*/
 
   return (
     <GlobalContext.Provider
