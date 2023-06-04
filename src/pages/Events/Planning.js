aeae15757dda54f83c3eace95a322bbdd5839e95
AntG2 <anth0n9g@gmail.com>
2023-06-04 15:47:21 -0700

    6/4: base past and upcoming events on database storage over local storage
    savedEvents is now a state instead of importing from GlobalContext.
    savedEvents extract data from realtime database storage
    (buggy) image upload by converting to base64 string and save in database

--- src/pages/Events/Planning.js
+++ src/pages/Events/Planning.js
@@ -1,66 +1,108 @@
 import React, { useContext, useEffect,  useState } from "react";
 import GlobalContext from "../../Context/GlobalContext";
 import dayjs from "dayjs";
-import { storage } from "../../utils/firebase";
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { auth, db } from "../../utils/firebase";
+import { ref, set, get, onValue } from "firebase/database";
 
 export default function Events({ lookForward }) {
-    const [pastEvents, setPastEvents] = useState([]);
-    const {savedEvents} = useContext(GlobalContext);
+    const [sortedEvents, setSortedEvents] = useState([]);
+    const [savedEvents, setSavedEvents] = useState([]);
     const day = dayjs();
-    
+
+
     useEffect(() => {
-	const events = savedEvents.filter(
-	    (event) => ((!lookForward) ? (dayjs(event.day).format("YYYYMMDD") < day.format("YYYYMMDD")) : (dayjs(event.day).format("YYYYMMDD") >= day.format("YYYYMMDD") )
-		       ));
-	events.sort(((lookForward) ? compareLT : compareGT));
-	setPastEvents(events);
+	const fetchData = async() => {
+	    const fetch = async () => {
+		try {
+		    const userEventsRef = ref(db, `Users/${auth.currentUser.uid}/Events`);
+		    const snapshot = await get(userEventsRef);
+
+		    const data = snapshot.val();
+		    const parsedEvents = data ? Object.values(data) : [];
+		    return parsedEvents;
+		} catch (error) {
+		    console.log("Error fetching user events:", error);
+		}
+	    };
+	    if (auth.currentUser) {
+		const parsedEvents = await fetch();
+		setSavedEvents(parsedEvents);
+	    }
+	};
+	fetchData();
     }, [day.format("YYYYMMDD")]);
     
-    const [file, setFile] = useState("");
-    const [percent, setPercent] = useState(0);
-
-    function handleChange(event) {
-	setFile(event.target.files[0]);
+    useEffect(() => {
+	const events = savedEvents.filter((event) =>
+	    ((!lookForward) ?
+	     (dayjs(event.day).format("YYYYMMDD") < day.format("YYYYMMDD"))
+	     :
+	     (dayjs(event.day).format("YYYYMMDD") >= day.format("YYYYMMDD"))
+	));
+	events.sort(((lookForward) ? compareLT : compareGT));
+	setSortedEvents(events);
+    }, [day.format("YYYYMMDD"), auth.currentUser, savedEvents]);
+    
+    const [file, setFile] = useState(null);
+    
+    function handleChange(e) {
+        setFile(e.target.files[0]);
     }
 
-    const handleUpload = () => {
-	if (!file) {
-	    alert("Please upload an image first!");
+    const handleImageUpload = async () => {
+	if (file) {
+	    const reader = new FileReader();
+	    reader.readAsDataURL(file);
+	    reader.onloadend = async () => {
+		const base64String = reader.result.split(',')[1];
+		const dbRef = ref(db, `Users/${auth.currentUser.uid}/Events/images`);
+		await set(dbRef, base64String);
+		console.log('Image uploaded successfully!');
+	    };
 	}
-	const storageRef = ref(storage, '/files/${file.name}');
-
-	const uploadTask = uploadBytesResumable(storageRef, file);
-
-	uploadTask.on(
-	    "state_changed",
-	    (snapshot) => {
-		const percent = Math.round(
-		    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-		);
-
-		setPercent(percent);
-	    },
-	    (err) => console.log(err),
-	    () => {
-		getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-		    console.log(url);
-		});
-	    }
-	);
     };
+
+    const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
+	const byteCharacters = atob(b64Data);
+	const byteArrays = [];
+	
+	for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+	    const slice = byteCharacters.slice(offset, offset + sliceSize);
+	    
+	    const byteNumbers = new Array(slice.length);
+	    for (let i = 0; i < slice.length; i++) {
+		byteNumbers[i] = slice.charCodeAt(i);
+	    }
+	    
+	    const byteArray = new Uint8Array(byteNumbers);
+	    byteArrays.push(byteArray);
+	}
+	    
+	const blob = new Blob(byteArrays, { type: contentType });
+	return blob;
+    };
+	
+   // const decodedImageBlob = b64toBlob(base64String, 'image/jpeg');
+   // const imageURL = URL.createObjectURL(decodedImageBlob);
+   // <img src="imageURL" alt="">
+    
+	
+    
     
     return (
 	<>
 	    <h1 className="flex flex-col items-center"> {(!lookForward) ? "Past Events": "Upcoming Events"} </h1>
-	    {pastEvents.map((event, index) => (
+	    {sortedEvents.map((event, index) => (
 	    <div key={index}>
 		{event.title} {dayjs(event.day).format("MM-DD-YYYY")}
+		{(!lookForward) ?
 		<div>
-		    <input type="file" onChange={handleChange} accept="/image/*" />
-		    <button onClick={handleUpload}>Upload to Firebase</button>
-		    <p>{percent} "% done"</p>
+		    <input type="file" onChange={handleChange} />
+		    <button onClick={handleImageUpload}>Upload Image</button>
 		</div>
+		 :
+		 null
+		}
 	    </div>
 	    ))}
 	</>
@@ -81,4 +123,5 @@ function compareLT( a, b )
 {
     return -(compareGT(a, b));
 }
-    
+
+
